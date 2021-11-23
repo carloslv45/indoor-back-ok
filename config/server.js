60e181e3be3737aa19e2885e1d558d1ce2dd1b4a
oliver charry <oliver.charry.216@unisabaneta.edu.co>
2021-11-22 22:49:01 -0600

    backend indoor app v0.1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5419ebda6dfb946843cbaf94bb8162ed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ae2bcbd-c4f0-454f-9cba-d523c7763e33'
+};
